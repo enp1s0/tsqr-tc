043a232e524857efafce95e8b518040aca8cdd76
mutsuki <mutsuki@momo86.net>
2021-01-31 16:57:38 +0900

    Add `copy_matrix_g2s`

--- src/batchedqr.cu
+++ src/batchedqr.cu
@@ -1,5 +1,6 @@
 #include <cstdint>
 #include <cutf/type.hpp>
+#include <cutf/math.hpp>
 
 namespace {
 
@@ -15,6 +16,39 @@ __device__ void fill_zero(T* const ptr) {
 	} else {
 		for (unsigned index = threadIdx.x; index < size; index += block_size) {
 			ptr[index] = cutf::type::cast<T>(0.0f);
+		}
+	}
+}
+
+// This function copies matrix data from global memory to shared memory
+// Ristrictions:
+// - smem_m == block_size
+template <unsigned block_size, unsigned smem_n, unsigned smem_ld, class SMEM_T, class GMEM_T>
+__device__ void copy_matrix_g2s(
+		SMEM_T* const smem,
+		const GMEM_T* const gmem_ptr, const std::size_t gmem_ld,
+		const std::size_t m, const std::size_t n
+		) {
+	if (m == block_size) {
+		unsigned i_n = 0;
+		for (; i_n < n; i_n++) {
+			const auto v = gmem_ptr[gmem_ld * i_n + threadIdx.x];
+			smem[smem_ld * i_n + threadIdx.x] = cutf::type::cast<SMEM_T>(v);
+		}
+		for (; i_n < smem_n; i_n++) {
+			smem[smem_ld * i_n + threadIdx.x] = cutf::type::cast<SMEM_T>(0.0f);
+		}
+	} else {
+		unsigned i_n = 0;
+		for (; i_n < n; i_n++) {
+			const auto v = cutf::type::cast<GMEM_T>(0.0f);
+			if (threadIdx.x < m) {
+				v = gmem_ptr[gmem_ld * i_n + threadIdx.x];
+			}
+			smem[smem_ld * i_n + threadIdx.x] = cutf::type::cast<SMEM_T>(v);
+		}
+		for (; i_n < smem_n; i_n++) {
+			smem[smem_ld * i_n + threadIdx.x] = cutf::type::cast<SMEM_T>(0.0f);
 		}
 	}
 }
