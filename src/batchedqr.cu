6737b72bcb8aa4b722aafcb1f41f414c8709ec1b
mutsuki <mutsuki@momo86.net>
2021-02-01 23:26:50 +0900

    Add `accumulate_vectors`

--- src/batchedqr.cu
+++ src/batchedqr.cu
@@ -70,4 +70,21 @@ __device__ COMPUTE_T compute_norm2(const T* const ptr, const unsigned size) {
 	}
 	return norm2;
 }
+
+// This function accumulates vectors on shared memory.
+// Restrictions:
+// count == block_size / war_size
+// output_ptr == inpute_ptr
+template <unsigned block_size, class T>
+__device__ void accumulate_vectors(T* const smem_vec_ptr, const unsigned vec_len) {
+	for (unsigned whole_vec_len = vec_len * block_size / warp_size; whole_vec_len > vec_len; whole_vec_len >>= 1) {
+		for (unsigned offset = 0; offset < whole_vec_len / 2; offset += block_size) {
+			const auto index = offset + threadIdx.x;
+			if (index > vec_len) break;
+
+			smem_vec_ptr[index] += smem_vec_ptr[index + whole_vec_len / 2];
+		}
+		__syncthreads();
+	}
+}
 } // noname namespace
