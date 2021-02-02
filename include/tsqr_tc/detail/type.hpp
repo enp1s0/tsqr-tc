eb7244ebd3de85f4eee240ef9ef22ad4a7e9ef57
mutsuki <mutsuki@momo86.net>
2021-02-02 22:29:05 +0900

    Remove `get_w_type`, `get_y_type`, `get_t_type` and `get_a_type`. Add `get_type`

--- include/tsqr_tc/detail/type.hpp
+++ include/tsqr_tc/detail/type.hpp
@@ -5,17 +5,7 @@ namespace mtk {
 namespace tsqr_tc {
 namespace detail {
 template <mtk::tsqr_tc::compute_mode::type compute_mode>
-struct get_w_type {using type = float;};
-
-template <mtk::tsqr_tc::compute_mode::type compute_mode>
-struct get_y_type {using type = float;};
-
-template <mtk::tsqr_tc::compute_mode::type compute_mode>
-struct get_t_type {using type = float;};
-
-template <mtk::tsqr_tc::compute_mode::type compute_mode>
-struct get_a_type {using type = float;};
-
+struct get_type {using type = float;};
 } // namespace detail
 } // namespace tsqr_tc
 } // namespace mtk
