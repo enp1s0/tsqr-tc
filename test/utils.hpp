6604ca5494cd9dd94a043fe677e2aeffe9488869
mutsuki <mutsuki@momo86.net>
2021-02-04 03:18:48 +0900

    Fix the arguments of `compute_residual_in_dp`

--- test/utils.hpp
+++ test/utils.hpp
@@ -7,9 +7,13 @@ namespace tsqr_tc {
 namespace test_utils {
 template <class T>
 double compute_residual_in_dp(
-		const T* const dR_ptr,
-		const T* const dW_ptr,
-		const T* const dY_ptr,
+		const T* const dR_ptr, const std::size_t ld_R,
+		const T* const dW_ptr, const std::size_t ld_W,
+		const T* const dY_ptr, const std::size_t ld_Y,
+		const T* const dA_ptr, const std::size_t ld_A,
+		const std::size_t m, const std::size_t n,
+		cublasHandle_t const cublas_handle
+		);
 		const std::size_t m, const std::size_t n,
 		cublasHandle_t const cublas_handle
 		);
