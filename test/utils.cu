2a2e64d076849a339a6f2ef1fcfe0ecc419c89af
mutsuki <mutsuki@momo86.net>
2021-02-13 14:02:49 +0900

    Add `mtk::tsqr_tc::test_utils::qr_cublas`

--- test/utils.cu
+++ test/utils.cu
@@ -183,3 +183,90 @@ double mtk::tsqr_tc::test_utils::compute_orthogonality_in_dp<float>(
 		const std::size_t, const std::size_t,
 		cublasHandle_t const
 		);
+
+// CUSOLVER QR
+namespace {
+template <class T>
+__global__ void cut_r_kernel(
+		T* const dst, const std::size_t ld_dst,
+		const T* const src, const std::size_t ld_src,
+		const std::size_t m, const std::size_t n) {
+	const auto tid = blockIdx.x * blockDim.x + threadIdx.x;
+
+	const auto x = tid / n;
+	const auto y = tid % n;
+
+	if(y > x) return;
+
+	dst[ld_dst * x + y] = src[ld_src * x + y];
+}
+
+template <class T>
+void cut_r(T* const dst, const std::size_t ld_dst,
+		const T* const src, const std::size_t ld_src,
+		const std::size_t m, const std::size_t n) {
+	constexpr std::size_t block_size = 256;
+	cut_r_kernel<T><<<(n * n + block_size - 1) / block_size, block_size>>>(
+			dst, ld_dst,
+			src, ld_src,
+			m, n
+			);
+}
+}
+
+template <class T>
+void mtk::tsqr_tc::test_utils::qr_cublas(
+		T* const dQ_ptr, const std::size_t ld_Q,
+		T* const dR_ptr, const std::size_t ld_R,
+		const T* const dA_ptr, const std::size_t ld_A,
+		const std::size_t m, const std::size_t n,
+		cusolverDnHandle_t const cusolver_handle
+		) {
+	convert_matrix<T, T>(
+			dQ_ptr, ld_Q,
+			dA_ptr, ld_A,
+			m, n
+			);
+	auto d_tau = cutf::memory::get_device_unique_ptr<T>(n * n);
+	int geqrf_working_memory_size, gqr_working_memory_size;
+	CUTF_CHECK_ERROR(cutf::cusolver::dn::geqrf_buffer_size(
+				cusolver_handle, m, n,
+				dQ_ptr, m, &geqrf_working_memory_size
+				));
+	CUTF_CHECK_ERROR(cutf::cusolver::dn::gqr_buffer_size(
+				cusolver_handle, m, n, n,
+				dQ_ptr, m, d_tau.get(), &gqr_working_memory_size
+				));
+
+	auto d_geqrf_working_memory = cutf::memory::get_device_unique_ptr<T>(geqrf_working_memory_size);
+	auto d_gqr_working_memory = cutf::memory::get_device_unique_ptr<T>(gqr_working_memory_size);
+	auto d_info = cutf::memory::get_device_unique_ptr<int>(1);
+
+	CUTF_CHECK_ERROR(cutf::cusolver::dn::geqrf(
+				cusolver_handle, m, n,
+				dQ_ptr, m, d_tau.get(), d_geqrf_working_memory.get(),
+				geqrf_working_memory_size, d_info.get()
+				));
+	cut_r(
+			dR_ptr, ld_R,
+			dQ_ptr, ld_A,
+			m, n
+			);
+
+	CUTF_CHECK_ERROR(cutf::cusolver::dn::gqr(
+				cusolver_handle, m, n, n,
+				dQ_ptr, m,
+				d_tau.get(), d_gqr_working_memory.get(), gqr_working_memory_size,
+				d_info.get()
+				));
+
+}
+
+template
+void mtk::tsqr_tc::test_utils::qr_cublas<float>(
+		float* const dQ_ptr, const std::size_t ld_Q,
+		float* const dR_ptr, const std::size_t ld_R,
+		const float* const dA_ptr, const std::size_t ld_A,
+		const std::size_t m, const std::size_t n,
+		cusolverDnHandle_t const cusolver_handle
+		);
--- test/utils.hpp
+++ test/utils.hpp
@@ -1,6 +1,7 @@
 #ifndef __MTK_TSQR_TC_TEST_UTILS_HPP__
 #define __MTK_TSQR_TC_TEST_UTILS_HPP__
 #include <cutf/cublas.hpp>
+#include <cutf/cusolver.hpp>
 
 namespace mtk {
 namespace tsqr_tc {
@@ -21,6 +22,15 @@ double compute_orthogonality_in_dp(
 		const T* const dY_ptr, const std::size_t ld_Y,
 		const std::size_t m, const std::size_t n,
 		cublasHandle_t const cublas_handle
+		);
+
+template <class T>
+void qr_cublas(
+		T* const dQ_ptr, const std::size_t ld_Q,
+		T* const dR_ptr, const std::size_t ld_R,
+		const T* const dA_ptr, const std::size_t ld_A,
+		const std::size_t m, const std::size_t n,
+		cusolverDnHandle_t const cusolver_handle
 		);
 } // namespace test_utils
 } // namespace tsqr_tc
