5e9358ae8b3d6e1112e73c1dfa88f97c92f54968
mutsuki <mutsuki@momo86.net>
2021-02-14 13:51:58 +0900

    Fix an argument name

--- include/tsqr_tc/batchedqr.hpp
+++ include/tsqr_tc/batchedqr.hpp
@@ -22,7 +22,7 @@ void qr256x128_batched(
 		typename mtk::tsqr_tc::detail::get_type<compute_mode>::type* const gmem_y_ptr, const std::size_t ldy,
 		typename mtk::tsqr_tc::detail::get_type<compute_mode>::type* const gmem_r_ptr, const std::size_t ldr,
 		const typename mtk::tsqr_tc::detail::get_type<compute_mode>::type* const gmem_a_ptr, const std::size_t lda,
-		const std::size_t m,
+		const std::size_t n,
 		const std::size_t batch_size,
 		const std::size_t* const start_m_list,
 		const cudaStream_t cuda_stream = 0
