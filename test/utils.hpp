4adf18005f080638d58c42a1b76f2e9f13ba0e2e
mutsuki <mutsuki@momo86.net>
2021-02-17 21:12:06 +0900

    Add `get_mode_name`

--- test/utils.hpp
+++ test/utils.hpp
@@ -2,10 +2,16 @@
 #define __MTK_TSQR_TC_TEST_UTILS_HPP__
 #include <cutf/cublas.hpp>
 #include <cutf/cusolver.hpp>
+#include <tsqr_tc/detail/constant.hpp>
+#include <string>
 
 namespace mtk {
 namespace tsqr_tc {
 namespace test_utils {
+template <mtk::tsqr_tc::compute_mode::type compute_mode>
+inline const char* get_mode_name();
+template <> inline const char* get_mode_name<mtk::tsqr_tc::compute_mode::fp32_hmma_cor>() {return "fp32_hmma_cor";}
+
 template <class T>
 double compute_residual_in_dp(
 		const T* const dR_ptr, const std::size_t ld_R,
