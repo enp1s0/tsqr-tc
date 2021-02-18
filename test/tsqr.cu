b6fe6c64b1bab2bc78661411a33f43e4e15a0ed4
mutsuki <mutsuki@momo86.net>
2021-02-18 22:33:06 +0900

    Add performance test

--- test/tsqr.cu
+++ test/tsqr.cu
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <random>
+#include <chrono>
 #include <tsqr_tc/tsqr.hpp>
 #include <cutf/memory.hpp>
 #include <cutf/cublas.hpp>
@@ -91,9 +92,61 @@ void test_accuracy(const std::size_t m, const std::size_t n, const unsigned test
 	std::fflush(stdout);
 }
 
+template <mtk::tsqr_tc::compute_mode::type compute_mode>
+void test_performance(const std::size_t m, const std::size_t n, const unsigned test_count) {
+	using compute_t = typename mtk::tsqr_tc::detail::get_type<compute_mode>::type;
+	auto hA_uptr = cutf::memory::get_host_unique_ptr<compute_t>(m * n);
+
+	auto dA_uptr = cutf::memory::get_device_unique_ptr<compute_t>(m * n);
+	auto dQ_uptr = cutf::memory::get_device_unique_ptr<compute_t>(m * n);
+	auto dR_uptr = cutf::memory::get_device_unique_ptr<compute_t>(n * n);
+
+	mtk::tsqr_tc::tsqr_buffer<compute_mode> tsqr_buffer(m, n);
+	tsqr_buffer.allocate();
+
+	// initialize input matrix
+	{
+		std::mt19937 mt(std::random_device{}());
+		std::uniform_real_distribution<float> dist(-rand_abs_max, rand_abs_max);
+		for (unsigned i = 0; i < m * n; i++) {
+			hA_uptr.get()[i] = cutf::type::cast<compute_t>(dist(mt));
+		}
+	}
+	cutf::memory::copy(dA_uptr.get(), hA_uptr.get(), m * n);
+
+	auto cuda_stream_uptr = cutf::stream::get_stream_unique_ptr();
+
+	const auto start_clock = std::chrono::high_resolution_clock::now();
+	for (unsigned c = 0; c < test_count; c++) {
+		mtk::tsqr_tc::tsqr(
+				dQ_uptr.get(), m,
+				dR_uptr.get(), n,
+				dA_uptr.get(), m,
+				m, n,
+				tsqr_buffer,
+				*cuda_stream_uptr.get()
+				);
+	}
+	CUTF_CHECK_ERROR(cudaDeviceSynchronize());
+	const auto end_clock = std::chrono::high_resolution_clock::now();
+
+	const double elapsed_time = std::chrono::duration_cast<std::chrono::microseconds>(end_clock - start_clock).count() * 1e-6 / test_count;
+
+	std::printf("%lu,%lu,%s,%e,%lu\n", m, n, mtk::tsqr_tc::test_utils::get_mode_name<compute_mode>(), elapsed_time, tsqr_buffer.get_buffer_size());
+	std::fflush(stdout);
+}
+
 int main() {
-	std::printf("m,n,mode,residual,orthogonality\n");
-	for (std::size_t lm = 10; lm < 23; lm++) {
-		test_accuracy<mtk::tsqr_tc::compute_mode::fp32_hmma_cor>(1lu << lm, 64, test_count);
+	std::printf("m,n,mode,time,buffer_size\n");
+	for (std::size_t lm = 10; lm < 20; lm++) {
+		test_accuracy<mtk::tsqr_tc::compute_mode::fp32_hmma_cor>(1lu << lm, 128, test_count);
+	}
+
+	for (std::size_t lm = 10; lm < 20; lm++) {
+		test_performance<mtk::tsqr_tc::compute_mode::fp32_hmma_cor>(1lu << lm, 128, test_count);
+	}
+
+	for (std::size_t lm = 10; lm < 20; lm++) {
+		mtk::tsqr_tc::test_utils::test_performance_cublas<float>(1lu << lm, 128, test_count);
 	}
 }
