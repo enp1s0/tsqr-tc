46bcd33f94c07ee6e63b78115c1fb8a57ae3216f
mutsuki <mutsuki@momo86.net>
2021-02-25 17:43:13 +0900

    Add compute mode `fp32_fp16_hmma_no_cor` and `fp32_tf32_hmma_no_cor`

--- include/tsqr_tc/detail/constant.hpp
+++ include/tsqr_tc/detail/constant.hpp
@@ -4,8 +4,10 @@ namespace mtk {
 namespace tsqr_tc {
 namespace compute_mode {
 using type = unsigned;
-static const type fp32_fp16_hmma_cor = 1;
-static const type fp32_tf32_hmma_cor = 2;
+static const type fp32_fp16_hmma_cor    = 1;
+static const type fp32_tf32_hmma_cor    = 2;
+static const type fp32_fp16_hmma_no_cor = 3;
+static const type fp32_tf32_hmma_no_cor = 4;
 } // namespace compute_mode
 } // namespace tsqr_tc
 } // namespace mtk
